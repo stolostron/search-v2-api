06088045d392bdf6d1c192a2bc491c4152460cc0
Jorge Padilla <jpadilla@redhat.com>
2022-01-20 09:48:35 -0500

    Merge relationships PR (#9)
    * WIP Merge relationships PR
    
    * Update schema.resolvers.go

--- pkg/resolver/search.go
+++ pkg/resolver/search.go
@@ -2,9 +2,9 @@ package resolver
 
 import (
 	"context"
-	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/driftprogramming/pgxpoolmock"
 	"github.com/lib/pq"
@@ -16,6 +16,8 @@ import (
 type SearchResult struct {
 	input *model.SearchInput
 	pool  pgxpoolmock.PgxPool
+	uids  []string       // List of uids from search result to be used to get relatioinships.
+	wg    sync.WaitGroup // WORKAROUND: Used to serialize search query and relatioinships query.
 	// 	Count   int
 	// 	Items   []map[string]interface{}
 	//  Related []SearchRelatedResult
@@ -46,6 +48,9 @@ func (s *SearchResult) Count() int {
 }
 
 func (s *SearchResult) Items() []map[string]interface{} {
+	s.wg.Add(1)
+	defer s.wg.Done()
+	klog.Info("Resolving SearchResult:Items()")
 	qString, qArgs := s.buildSearchQuery(context.Background(), false)
 	r, e := s.resolveItems(qString, qArgs)
 	if e != nil {
@@ -55,8 +60,15 @@ func (s *SearchResult) Items() []map[string]interface{} {
 }
 
 func (s *SearchResult) Related() []SearchRelatedResult {
-	fmt.Printf("Resolving SearchResult:Related() - input: %+v\n", s.input)
-	r := make([]SearchRelatedResult, 1)
+	klog.Info("Resolving SearchResult:Related()")
+
+	// FIXME: WORKAROUND when the query doesn't request Items() we must use a more efficient query to get the uids.
+	if s.uids == nil {
+		s.Items()
+	}
+	s.wg.Wait() // WORKAROUND wait to complete execution of Items()
+
+	r := s.getRelations()
 	return r
 }
 
@@ -128,15 +140,19 @@ func (s *SearchResult) resolveCount(query string, args []interface{}) (int, erro
 }
 
 func (s *SearchResult) resolveItems(query string, args []interface{}) ([]map[string]interface{}, error) {
-	rows, _ := s.pool.Query(context.Background(), query, args...)
-	//TODO: Handle error
+	rows, err := s.pool.Query(context.Background(), query, args...)
+	if err != nil {
+		klog.Errorf("Error resolving query [%s] with args [%+v]. Error: [%+v]", query, args, err)
+	}
 	defer rows.Close()
+
 	var uid, cluster string
 	var data map[string]interface{}
 	items := []map[string]interface{}{}
+	s.uids = make([]string, len(items))
 
 	for rows.Next() {
-		err := rows.Scan(&uid, &cluster, &data)
+		err = rows.Scan(&uid, &cluster, &data)
 		if err != nil {
 			klog.Errorf("Error %s retrieving rows for query:%s", err.Error(), query)
 		}
@@ -158,8 +174,111 @@ func (s *SearchResult) resolveItems(query string, args []interface{}) ([]map[str
 		currCluster := cluster
 		currItem["cluster"] = currCluster
 		items = append(items, currItem)
+		s.uids = append(s.uids, currUid)
 	}
 	klog.Info("len items: ", len(items))
 
 	return items, nil
+}
+
+func (s *SearchResult) getRelations() []SearchRelatedResult {
+	klog.Infof("Resolving relationships for [%d] uids.\n", len(s.uids))
+
+	//defining variables
+	items := []map[string]interface{}{}
+	var kindSlice []string
+	var kindList []string
+	var countList []int
+
+	// LEARNING: IN is equivalent to = ANY and performance is not deteriorated when we replace IN with =ANY
+	recrusiveQuery := `with recursive
+	search_graph(uid, data, sourcekind, destkind, sourceid, destid, path, level)
+	as (
+	SELECT r.uid, r.data, e.sourcekind, e.destkind, e.sourceid, e.destid, ARRAY[r.uid] as path, 1 as level
+		from search.resources r
+		INNER JOIN
+			search.edges e ON (r.uid = e.sourceid) or (r.uid = e.destid)
+		 where r.uid = ANY($1)
+	union
+	select r.uid, r.data, e.sourcekind, e.destkind, e.sourceid, e.destid, path||r.uid, level+1 as level
+		from search.resources r
+		INNER JOIN
+			search.edges e ON (r.uid = e.sourceid)
+		, search_graph sg
+		where (e.sourceid = sg.destid or e.destid = sg.sourceid)
+		and r.uid <> all(sg.path)
+		and level = 1 
+		)
+	select distinct on (destid) data, destid, destkind from search_graph where level=1 or destid = ANY($2)`
+
+	relations, QueryError := s.pool.Query(context.Background(), recrusiveQuery, s.uids, s.uids) // how to deal with defaults.
+	if QueryError != nil {
+		klog.Errorf("query error :", QueryError)
+	}
+
+	defer relations.Close()
+
+	// iterating through resulting rows and scaning data, destid  and destkind
+	for relations.Next() {
+		var destkind, destid string
+		var data map[string]interface{}
+		relatedResultError := relations.Scan(&data, &destid, &destkind)
+		if relatedResultError != nil {
+			klog.Errorf("Error %s retrieving rows for relationships:%s", relatedResultError.Error(), relations)
+		}
+		// creating currItem variable to keep data and converting strings in data to lowercase
+		currItem := make(map[string]interface{})
+		for k, myInterface := range data {
+			switch v := myInterface.(type) {
+			case string:
+				currItem[k] = strings.ToLower(v)
+			default:
+				// klog.Info("Not string type.", k, v)
+				continue
+			}
+		}
+		// creating currKind variable to store kind and appending to list
+		currKind := destkind
+		currItem["Kind"] = currKind
+		kindSlice = append(kindSlice, currKind)
+		items = append(items, currItem)
+
+	}
+	// saving relationships:
+	// json_items, _ := json.Marshal(items)
+	// fmt.Println("All related results", string(json_items))
+	// ioutil.WriteFile("rel_data.json", json_items, os.ModePerm)
+
+	//calling function to get map which contains unique values from kindSlice and counts the number occurances ex: map[key:Pod, value:2] if pod occurs 2x in kindSlice
+	count := printUniqueValue(kindSlice)
+
+	//iterating over count and appending to new lists (kindList and countList)
+	for k, v := range count {
+		// fmt.Println("Keys:", k)
+		kindList = append(kindList, k)
+		// fmt.Println("Values:", v)
+		countList = append(countList, v)
+	}
+
+	//instantiating composite literal
+	relatedSearch := make([]SearchRelatedResult, len(count))
+
+	//iterating and sending values to relatedSearch
+	for i := range kindList {
+		kind := kindList[i]
+		count := countList[i]
+		relatedSearch[i] = SearchRelatedResult{kind, &count, items}
+	}
+
+	return relatedSearch
+}
+
+//helper function TODO: make helper.go module to store these if needed.
+func printUniqueValue(arr []string) map[string]int {
+	//Create a   dictionary of values for each element
+	dict := make(map[string]int)
+	for _, num := range arr {
+		dict[num] = dict[num] + 1
+	}
+	return dict
 }
