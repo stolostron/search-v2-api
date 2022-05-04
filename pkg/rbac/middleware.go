8e31aeb90c31f3b36753cb3f7384a81560c9aa4e
Anxhela <acoba@redhat.com>
2022-05-04 12:54:13 -0400

    [WIP] authorizing user impersonation
    Signed-off-by: Anxhela <acoba@redhat.com>

--- pkg/rbac/middleware.go
+++ pkg/rbac/middleware.go
@@ -3,12 +3,17 @@ package rbac
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/stolostron/search-v2-api/pkg/config"
 	authv1 "k8s.io/api/authentication/v1"
+	authov1 "k8s.io/api/authorization/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
 )
 
@@ -31,13 +36,15 @@ func Middleware() func(http.Handler) http.Handler {
 			// Retrieving and verifying the token
 			if clientToken == "" {
 				klog.V(4).Info("Request didn't have a valid authentication token.")
-				http.Error(w, "{\"message\":\"Request didn't have a valid authentication token.\"}", http.StatusUnauthorized)
+				http.Error(w, "{\"message\":\"Request didn't have a valid authentication token.\"}",
+					http.StatusUnauthorized)
 				return
 			}
-			authenticated, err := verifyToken(clientToken, r.Context())
+			authenticated, uid, err := verifyToken(clientToken, r.Context())
 			if err != nil {
 				klog.Warning("Unexpected error while authenticating the request token.", err)
-				http.Error(w, "{\"message\":\"Unexpected error while authenticating the request token.\"}", http.StatusInternalServerError)
+				http.Error(w, "{\"message\":\"Unexpected error while authenticating the request token.\"}",
+					http.StatusInternalServerError)
 				return
 			}
 			if !authenticated {
@@ -47,13 +54,19 @@ func Middleware() func(http.Handler) http.Handler {
 			}
 
 			klog.V(5).Info("User authentication successful!")
+
+			// //2.check that authenticated users impersonation privilages (authorize):
+			if authorize(uid, r.Context()) {
+				klog.V(5).Info("Authorized")
+			}
+
 			next.ServeHTTP(w, r)
 
 		})
 	}
 }
 
-func verifyToken(clientId string, ctx context.Context) (bool, error) {
+func verifyToken(clientId string, ctx context.Context) (bool, types.UID, error) {
 	tr := authv1.TokenReview{
 		Spec: authv1.TokenReviewSpec{
 			Token: clientId,
@@ -62,14 +75,59 @@ func verifyToken(clientId string, ctx context.Context) (bool, error) {
 	result, err := config.KubeClient().AuthenticationV1().TokenReviews().Create(ctx, &tr, metav1.CreateOptions{})
 	if err != nil {
 		klog.Warning("Error creating the token review.", err.Error())
-		return false, err
+		return false, "", err
 	}
 	klog.V(9).Infof("%v\n", prettyPrint(result.Status))
 	if result.Status.Authenticated {
-		return true, nil
+		return true, result.UID, nil
 	}
 	klog.V(4).Info("User is not authenticated.") //should this be warning or info?
-	return false, nil
+	return false, "", nil
+}
+
+func authorize(uid types.UID, ctx context.Context) bool { //we want to return the SelfSubjectAccessReviewSpec{}
+
+	//create impersonation config that will impersonates user based on UID (from tokereview):
+	config, _ := config.GetClientConfig()
+	config.Impersonate = rest.ImpersonationConfig{
+		UID: string(uid),
+	}
+	//create a new clientset for the impersonation
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Warning("Error with creating a new clientset with impersonation config.", err.Error())
+	}
+
+	// SelfSubjectAccessReview checks whether or not the current user can perform an action.
+	// Not filling in a spec.namespace means "in all namespaces". Self is a special case,
+	// because users should always be able to check whether they can perform an action
+	checkSelf := authov1.SelfSubjectAccessReview{
+		Spec: authov1.SelfSubjectAccessReviewSpec{
+			ResourceAttributes: &authov1.ResourceAttributes{
+				Verb:     "list",
+				Resource: "pods",
+			},
+		},
+	}
+
+	result, err := clientset.AuthorizationV1().
+		SelfSubjectAccessReviews().
+		Create(ctx, &checkSelf, metav1.CreateOptions{})
+
+	fmt.Println(result)
+
+	if err != nil {
+		klog.Warning("Error creating the access review", err.Error())
+	}
+
+	//Status is filled in by the server and indicates whether the request is allowed or not
+	if !result.Status.Allowed {
+		klog.V(5).Info("Impersonation denied")
+		return false
+	} else {
+		klog.V(5).Info("Impersonation allowed")
+		return true
+	}
 }
 
 // https://stackoverflow.com/a/51270134
