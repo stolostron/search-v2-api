16ff2c34b830406afbefbaa3e0f654bc52bbd9c8
Anxhela <acoba@redhat.com>
2022-05-27 12:07:51 -0400

    unit test for token retrieval middleware
    Signed-off-by: Anxhela <acoba@redhat.com>

--- pkg/rbac/authenticationMiddleware.go
+++ pkg/rbac/authenticationMiddleware.go
@@ -9,43 +9,41 @@ import (
 )
 
 // verifies token (userid) with the TokenReview:
-func AuthenticateUser() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// if there is cookie available use that else use the authorization header:
-			var clientToken string
-			cookie, err := r.Cookie("acm-access-token-cookie")
-			if err == nil {
-				clientToken = cookie.Value
-				klog.V(6).Info("Got user token from Cookie.")
-			} else if r.Header.Get("Authorization") != "" {
-				klog.V(6).Info("Got user token from Authorization header.")
-				clientToken = r.Header.Get("Authorization")
-				// Remove the keyword "Bearer " if it exists in the header.
-				clientToken = strings.Replace(clientToken, "Bearer ", "", 1)
-			}
-			// Retrieving and verifying the token
-			if clientToken == "" {
-				klog.V(4).Info("Request didn't have a valid authentication token.")
-				http.Error(w, "{\"message\":\"Request didn't have a valid authentication token.\"}", http.StatusUnauthorized)
-				return
-			}
+func AuthenticateUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// if there is cookie available use that else use the authorization header:
+		var clientToken string
+		cookie, err := r.Cookie("acm-access-token-cookie")
+		if err == nil {
+			clientToken = cookie.Value
+			klog.V(6).Info("Got user token from Cookie.")
+		} else if r.Header.Get("Authorization") != "" {
+			klog.V(6).Info("Got user token from Authorization header.")
+			clientToken = r.Header.Get("Authorization")
+			// Remove the keyword "Bearer " if it exists in the header.
+			clientToken = strings.Replace(clientToken, "Bearer ", "", 1)
+		}
+		// Retrieving and verifying the token
+		if clientToken == "" {
+			klog.V(4).Info("Request didn't have a valid authentication token.")
+			http.Error(w, "{\"message\":\"Request didn't have a valid authentication token.\"}", http.StatusUnauthorized)
+			return
+		}
 
-			authenticated, err := cache.IsValidToken(r.Context(), clientToken)
-			if err != nil {
-				klog.Warning("Unexpected error while authenticating the request token.", err)
-				http.Error(w, "{\"message\":\"Unexpected error while authenticating the request token.\"}", http.StatusInternalServerError)
-				return
-			}
-			if !authenticated {
-				klog.V(4).Info("Rejecting request: Invalid token.")
-				http.Error(w, "{\"message\":\"Invalid token\"}", http.StatusForbidden)
-				return
-			}
+		authenticated, err := cache.IsValidToken(r.Context(), clientToken)
+		if err != nil {
+			klog.Warning("Unexpected error while authenticating the request token.", err)
+			http.Error(w, "{\"message\":\"Unexpected error while authenticating the request token.\"}", http.StatusInternalServerError)
+			return
+		}
+		if !authenticated {
+			klog.V(4).Info("Rejecting request: Invalid token.")
+			http.Error(w, "{\"message\":\"Invalid token\"}", http.StatusForbidden)
+			return
+		}
 
-			klog.V(6).Info("User authentication successful!")
-			next.ServeHTTP(w, r)
+		klog.V(6).Info("User authentication successful!")
+		next.ServeHTTP(w, r)
 
-		})
-	}
+	})
 }
--- pkg/rbac/authenticationMiddleware_test.go
+++ pkg/rbac/authenticationMiddleware_test.go
@@ -1,2 +1,108 @@
 // Copyright Contributors to the Open Cluster Management project
 package rbac
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// var token string
+
+// func makeFakeToken() string {
+// 	t := make([]byte, 4)
+// 	rand.Read(t)
+// 	return fmt.Sprintf("%x", t)
+// }
+
+// // token from cookie provided - should return status okay
+func TestTokenCookieAuthenticated(t *testing.T) {
+
+	token := "validtoken"
+
+	authenticateHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	r := httptest.NewRequest("GET", "https://localhost:4010/playground", nil)
+
+	r.AddCookie(&http.Cookie{Name: "acm-access-token-cookie", Value: token})
+
+	response := httptest.NewRecorder()
+
+	authenticateHandler(response, r)
+	authen := AuthenticateUser(authenticateHandler)
+
+	authen.ServeHTTP(response, r)
+	assert.Equal(t, response.Code, http.StatusOK)
+
+}
+
+// token from header provided - should return status okay (but doesn't)
+func TestAuthenticateHeaderUser(t *testing.T) {
+
+	token := "validtoken"
+
+	authenticateHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	r := httptest.NewRequest("GET", "https://localhost:4010/playground", nil)
+
+	r.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+	response := httptest.NewRecorder()
+
+	authenticateHandler(response, r)
+	authen := AuthenticateUser(authenticateHandler)
+
+	authen.ServeHTTP(response, r)
+	assert.Equal(t, response.Code, http.StatusOK)
+}
+
+// Invalid token provided - should return unauthorized
+func TestAuthenticateInvalidHeaderUser(t *testing.T) {
+
+	authenticateHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	r := httptest.NewRequest("GET", "https://localhost:4010/playground", nil)
+
+	r.Header.Add("Authorization", fmt.Sprintf("Bearer %v", 123))
+	response := httptest.NewRecorder()
+
+	authenticateHandler(response, r)
+	authen := AuthenticateUser(authenticateHandler)
+
+	authen.ServeHTTP(response, r)
+	assert.Equal(t, response.Code, http.StatusUnauthorized)
+
+}
+
+// No token provided - should return unauthorized
+func TestAuthenticateNoTokenUser(t *testing.T) {
+	authenticateHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	r := httptest.NewRequest("GET", "https://localhost:4010/playground", nil)
+
+	response := httptest.NewRecorder()
+
+	authenticateHandler(response, r)
+	authen := AuthenticateUser(authenticateHandler)
+	authen.ServeHTTP(response, r)
+	assert.Equal(t, response.Code, http.StatusUnauthorized)
+}
+
+// Empty token provided - should return unauthorized
+func TestAuthenticateEmptyTokenUser(t *testing.T) {
+
+	authenticateHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	r := httptest.NewRequest("GET", "https://localhost:4010/playground", nil)
+
+	r.Header.Add("Authorization", fmt.Sprintf("Bearer %v", ""))
+	response := httptest.NewRecorder()
+
+	authenticateHandler(response, r)
+	authen := AuthenticateUser(authenticateHandler)
+
+	authen.ServeHTTP(response, r)
+	assert.Equal(t, response.Code, http.StatusUnauthorized)
+}
