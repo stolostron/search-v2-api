2571eb49bd3b58d0f9084359d44d1d8a9b4e7fc7
Jorge Padilla <jpadilla@redhat.com>
2022-06-07 17:20:06 -0400

    Fix tests
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/rbac/tokenReview.go
+++ pkg/rbac/tokenReview.go
@@ -19,64 +19,66 @@ type tokenReviewCache struct {
 	err         error
 	lock        sync.Mutex
 	updatedAt   time.Time
+	parentCache *Cache // Reference back to parent cache.
 	token       string
 	tokenReview *authv1.TokenReview
 }
 
 // Verify that the token is valid using a TokenReview.
 // Will use cached data if available and valid, otherwise starts a new request.
-func (cache *Cache) IsValidToken(ctx context.Context, token string) (bool, error) {
-	tr, err := cache.getTokenReview(ctx, token)
+func (c *Cache) IsValidToken(ctx context.Context, token string) (bool, error) {
+	tr, err := c.getTokenReview(ctx, token)
 	return tr.Status.Authenticated, err
 }
 
 // Get the TokenReview response for a given token.
 // Will use cached data if available and valid, otherwise starts a new request.
-func (cache *Cache) getTokenReview(ctx context.Context, token string) (*authv1.TokenReview, error) {
-	cache.tokenReviewsLock.Lock()
-	defer cache.tokenReviewsLock.Unlock()
+func (c *Cache) getTokenReview(ctx context.Context, token string) (*authv1.TokenReview, error) {
+	c.tokenReviewsLock.Lock()
+	defer c.tokenReviewsLock.Unlock()
 
 	// Check if a TokenReviewCacheRequest exists in the cache or create a new one.
-	cachedTR, tokenExists := cache.tokenReviews[token]
+	cachedTR, tokenExists := c.tokenReviews[token]
 	if !tokenExists {
 		cachedTR = &tokenReviewCache{
-			token: token,
+			parentCache: c,
+			token:       token,
 		}
-		cache.tokenReviews[token] = cachedTR
+		c.tokenReviews[token] = cachedTR
 	}
 	return cachedTR.getTokenReview()
 }
 
 // Get the resolved TokenReview from the cached tokenReviewCachedRequest object.
-func (trr *tokenReviewCache) getTokenReview() (*authv1.TokenReview, error) {
+func (trc *tokenReviewCache) getTokenReview() (*authv1.TokenReview, error) {
 	// This ensures that only 1 process is updating the TokenReview data from API request.
-	trr.lock.Lock()
-	defer trr.lock.Unlock()
+	trc.lock.Lock()
+	defer trc.lock.Unlock()
 
 	// Check if cached TokenReview data is valid. Update if needed.
-	if time.Now().After(trr.updatedAt.Add(time.Duration(config.Cfg.AuthCacheTTL) * time.Millisecond)) {
-		klog.V(5).Infof("Resolving TokenReview. tokenReviewCacheRequest expired or never updated. Last update %s", trr.updatedAt)
+	if time.Now().After(trc.updatedAt.Add(time.Duration(config.Cfg.AuthCacheTTL) * time.Millisecond)) {
+		klog.V(5).Infof("Resolving TokenReview. tokenReviewCacheRequest expired or never updated. Last update %s", trc.updatedAt)
 
 		tr := authv1.TokenReview{
 			Spec: authv1.TokenReviewSpec{
-				Token: trr.token,
+				Token: trc.token,
 			},
 		}
 
-		result, err := cache.getAuthClient().TokenReviews().Create(context.TODO(), &tr, metav1.CreateOptions{})
+		result, err := trc.parentCache.getAuthClient().TokenReviews().Create(context.TODO(), &tr, metav1.CreateOptions{})
 		if err != nil {
 			klog.Warning("Error resolving TokenReview from Kube API.", err.Error())
 		}
 		klog.V(9).Infof("TokenReview Kube API result: %v\n", prettyPrint(result.Status))
 
-		trr.updatedAt = time.Now()
-		trr.err = err
-		trr.tokenReview = result
+		trc.updatedAt = time.Now()
+		trc.err = err
+		trc.tokenReview = result
 	} else {
 		klog.V(6).Info("Using cached TokenReview.")
 	}
 
-	return trr.tokenReview, trr.err
+	return trc.tokenReview, trc.err
 }
 
 // https://stackoverflow.com/a/51270134
@@ -86,9 +88,9 @@ func prettyPrint(i interface{}) string {
 }
 
 // Utility to allow tests to inject a fake client to mock the k8s api call.
-func (cache *Cache) getAuthClient() v1.AuthenticationV1Interface {
-	if cache.authClient == nil {
-		cache.authClient = config.KubeClient().AuthenticationV1()
+func (c *Cache) getAuthClient() v1.AuthenticationV1Interface {
+	if c.authClient == nil {
+		c.authClient = config.KubeClient().AuthenticationV1()
 	}
-	return cache.authClient
+	return c.authClient
 }
--- pkg/rbac/tokenReview_test.go
+++ pkg/rbac/tokenReview_test.go
@@ -68,8 +68,9 @@ func Test_IsValidToken_expiredCache(t *testing.T) {
 	// Initialize cache and set state to TokenReview updated 5 minutes ago.
 	mock_cache := newCache()
 	mock_cache.tokenReviews["1234567890-expired"] = &tokenReviewCache{
-		updatedAt: time.Now().Add(time.Duration(-5) * time.Minute),
-		token:     "1234567890-expired",
+		parentCache: &mock_cache,
+		updatedAt:   time.Now().Add(time.Duration(-5) * time.Minute),
+		token:       "1234567890-expired",
 		tokenReview: &authv1.TokenReview{
 			Status: authv1.TokenReviewStatus{
 				Authenticated: true,
