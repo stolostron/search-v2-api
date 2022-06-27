3ba0454474896300ee15760f75bcb927b085100e
Jorge Padilla <jpadilla@redhat.com>
2022-06-27 13:14:02 -0400

    Complete queries for scale tests
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- test/locust-users.py
+++ test/locust-users.py
@@ -8,55 +8,76 @@ userCount = 0
 token_string = "Replace with <oc whoami -t>"
 
 class UserBehavior(TaskSet):
-
     @task
     def do_keyword_search(self):
-        q = {
+        self.do_post({
+            "op": "searchByKeyword",
             "query": "query q($input: [SearchInput]) {\n  searchResult: search(input: $input) {\n    items\n    __typename\n  }\n}",
             "variables": {"input":[{"keywords": ["prom"]}]}
-        }
+        })
 
-        self.client.payload = q
-        self.do_post()
-        print("Sent keyword search for user {}".format(self.user.name))  
 
     @task
     def do_filter_search(self):
-        q = {
+        self.do_post({
+            "op": "searchByFilter",
             "query": "query q($input: [SearchInput]) {\n  searchResult: search(input: $input) {\n    items\n    __typename\n  }\n}",
             "variables": {"input":[{"filters": [{"property":"kind", "values":["Pod"]}]}]}
-        }
-        self.client.payload = q
-        self.do_post()
-        print("Sent filter search for user {}".format(self.user.name))        
+        })
+
 
     @task
     def do_count_search(self):
-        q = {
+        self.do_post({
+            "op": "searchByCount",
             "query": "query q($input: [SearchInput]) {\n  searchResult: search(input: $input) {\n    count\n    __typename\n  }\n}",
             "variables": {"input":[
                 {"filters": [{"property":"kind", "values":["Pod"]}]},
                 {"filters": [{"property":"kind", "values":["Deployment"]}]},
             ]}
-        }
-        self.client.payload = q
-        self.do_post()
-        print("Sent count search for user {}".format(self.user.name))
+        })
 
-    # @task
-    # def do_related_search(self):
-    #     print("TODO: Send related query.")
 
-    # @task
-    # def do_autocomplete(self):
-    #     print("TODO: Send autocomplete query.")
+    @task
+    def do_autocomplete(self):
+        self.do_post({
+            "op": "searchComplete",
+            "query": "query q($property: String!, $query: SearchInput, $limit: Int) {\n  searchComplete(property: $property, query: $query, limit: $limit)\n}",
+            "variables": {"property": "name"},
+        })
 
-    def do_post(self):
-        self.client.post("/searchapi/graphql", 
+    @task
+    def do_search_related_count(self):
+        self.do_post({
+            "op": "searchRelatedCount",
+            "query": "query q($input: [SearchInput]) {\n  searchResult: search(input: $input) {\n    related {\n      kind\n      count\n      __typename\n    }\n    __typename\n  }\n}",
+            "variables": {"input":[
+                {"filters": [{"property": "kind","values": ["Pod"]}]},
+            ]}
+        })
+
+    @task
+    def do_search_related_items(self):
+        self.do_post({
+            "op": "searchRelatedItems",
+            "query": "query q($input: [SearchInput]) {\n  searchResult: search(input: $input) {\n    related {\n      kind\n      items\n      __typename\n    }\n    __typename\n  }\n}",
+            "variables": {"input":[
+                {"filters": [{"property": "kind","values": ["Pod"]}]},
+            ]}
+        })
+
+
+    def do_post(self, payload):
+        # Note: the query parameter op is a hack to report graphql queries separately. This is ignored by the search api.
+        response = self.client.post("/searchapi/graphql?op={}".format(payload["op"]), 
             headers={"Authorization": "Bearer " + self.user.token},
-            json=self.client.payload, verify=False)
-
-       
+            json=payload, verify=False)
+        # Enable the following line to debug the requests from the terminal.
+        # print("Sent {} for user {}".format(payload["op"], self.user.name))
+        
+        # Enable the following line if you want to validate the response content.
+        # print("Response content: {}".format(response.content)) 
+    
 
 class User(HttpUser):
     name = ""
