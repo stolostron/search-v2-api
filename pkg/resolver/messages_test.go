495e909f2f9c34de609496797682749c115c1f59
Jorge Padilla <jpadilla@redhat.com>
2022-09-22 23:05:23 -0400

    Cleanup
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/resolver/messages_test.go
+++ pkg/resolver/messages_test.go
@@ -58,11 +58,11 @@ func Test_Messages_Error(t *testing.T) {
 	}
 }
 
-func Test_Message_Results_ValidCache(t *testing.T) {
+func Test_Messages_MultipleDisabledClusters(t *testing.T) {
 	// Build mock
 	mockMessage := Message{
 		cache: &MockCache{
-			disabled: map[string]struct{}{"managed1": {}},
+			disabled: map[string]struct{}{"managed1": {}, "managed2": {}},
 			err:      nil,
 		},
 	}
@@ -76,7 +76,7 @@ func Test_Message_Results_ValidCache(t *testing.T) {
 	message := model.Message{ID: "S20",
 		Kind:        &kind,
 		Description: &desc}
-	messages = append(messages, &message)
+	messages := append(messages, &message)
 
 	if !reflect.DeepEqual(messages, res) {
 		t.Errorf("Message results doesn't match. Expected: %#v, Got: %#v", messages, res)
@@ -86,8 +86,8 @@ func Test_Message_Results_ValidCache(t *testing.T) {
 	}
 }
 
-// user does not have access to disabled clusters
-func Test_Message_Results_NoAccessToDisabledC(t *testing.T) {
+// user does not have access to any disabled clusters
+func Test_Message_NoDisabledClusters(t *testing.T) {
 	// Build mock
 	mockMessage := Message{
 		cache: &MockCache{
@@ -106,35 +106,3 @@ func Test_Message_Results_NoAccessToDisabledC(t *testing.T) {
 		t.Errorf("Incorrect results. expected error to be [%v] got [%v]", nil, err)
 	}
 }
-
-//Return error running query
-// func Test_Message_Results_ErrRunningQuery(t *testing.T) {
-// 	csRes, nsRes, mc := newUserData()
-// 	ud := rbac.UserData{CsResources: csRes, NsResources: nsRes, ManagedClusters: mc}
-// 	// rbac.CacheInst.SetDisabledClusters(map[string]struct{}{}, fmt.Errorf("error"))
-// 	// Create a SearchSchemaResolver instance with a mock connection pool.
-// 	resolver, mockPool := newMockMessage(t, &ud)
-// 	rbac.CacheInst = *rbac.NewMockCacheForMessages(nil, nil, mockPool)
-
-// 	// Mock the database queries.
-// 	mockRows := newMockRowsWithoutRBAC("../resolver/mocks/mock.json", nil, "srchAddonDisabledCluster", 0)
-
-// 	// Mock the database query - return error
-// 	mockPool.EXPECT().Query(gomock.Any(),
-// 		gomock.Eq(`SELECT DISTINCT "mcInfo".data->>'name' AS "srchAddonDisabledCluster" FROM "search"."resources" AS "mcInfo" LEFT OUTER JOIN "search"."resources" AS "srchAddon" ON (("mcInfo".data->>'name' = "srchAddon".data->>'namespace') AND ("srchAddon".data->>'kind' = 'ManagedClusterAddOn') AND ("srchAddon".data->>'name' = 'search-collector')) WHERE (("mcInfo".data->>'kind' = 'ManagedClusterInfo') AND ("srchAddon".uid IS NULL) AND ("mcInfo".data->>'name' != 'local-cluster'))`),
-// 	).Return(mockRows, fmt.Errorf("err running query"))
-
-// 	ctx := context.WithValue(context.Background(), rbac.ContextAuthTokenKey, "123456")
-
-// 	//Execute the function
-// 	res, errRes := resolver.messageResults(ctx)
-
-// 	messages := make([]*model.Message, 0)
-
-// 	if !reflect.DeepEqual(messages, res) {
-// 		t.Errorf("Message results doesn't match. Expected: %#v, Got: %#v", messages, res)
-// 	}
-// 	if errRes == nil {
-// 		t.Errorf("Incorrect results. expected error to be [%v] got [%v]", fmt.Errorf("err running query"), errRes)
-// 	}
-// }
