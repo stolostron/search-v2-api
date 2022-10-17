45bbeceaa908abb0c695eb8544a40b5cee4af7c3
Jorge Padilla <jpadilla@redhat.com>
2022-10-17 10:32:27 -0400

    Fixes null related items and case of kind property (#103)
    * Fixes null related items and case of kind property
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Review comment and refactor
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update tests
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Add mock data
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Fix typo
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Reduce test output
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/resolver/related.go
+++ pkg/resolver/related.go
@@ -14,59 +14,54 @@ import (
 )
 
 type SearchRelatedResult struct {
-	// input *model.SearchInput //nolint: unused,structcheck
 	Kind  string                   `json:"kind"`
 	Count *int                     `json:"count"`
 	Items []map[string]interface{} `json:"items"`
 }
 
 // func (s *SearchRelatedResult) Count() int {
-// 	klog.Info("TODO: Resolve SearchRelatedResult: Count() - model/related.go")
 // 	return 0
 // }
-
-// func (s *SearchRelatedResult) Kind() string {
-// 	klog.Info("TODO: Resolve SearchRelatedResult: Kind()  - model/related.go")
-// 	return "TODO:Kind"
-// }
-
 // func (s *SearchRelatedResult) Items() []map[string]interface{} {
-// 	klog.Info("TODO: Resolve SearchRelatedResult: Items() - model/related.go")
 // 	return nil
 // }
+
+// Builds the database query to get relationships for the items in the search result.
 func (s *SearchResult) buildRelationsQuery() {
-	// Example query to find relations between resources - accepts an array of uids
-	// =============================================================================
-	// 	SELECT "uid", "kind", MIN("level") AS "level" FROM
-	// (
-	// 	SELECT "level", unnest(array[sourceid, destid, concat('cluster__',cluster)]) AS "uid",
-	// unnest(array[sourcekind, destkind, 'Cluster']) AS "kind"
-	// 	FROM (
-	// 		WITH RECURSIVE search_graph(level, sourceid, destid,  sourcekind, destkind, cluster) AS
-	// 		(SELECT 1 AS "level", "sourceid", "destid", "sourcekind", "destkind", "cluster"
-	// 		 FROM "search"."edges" AS "e"
-	// 		 WHERE (("destid" IN ('local-cluster/108a77a2-159c-4621-ae1e-7a3649000ebc' )) OR
-	// 					("sourceid" IN ('local-cluster/108a77a2-159c-4621-ae1e-7a3649000ebc'))
-	// 			   )
-	// 				   UNION
-	// 		 (SELECT level+1 AS "level", "e"."sourceid", "e"."destid", "e"."sourcekind", "e"."destkind", "e"."cluster"
-	// 		  FROM "search"."edges" AS "e"
-	// 		  INNER JOIN "search_graph" AS "sg"
-	// 		  ON (("sg"."destid" IN ("e"."sourceid", "e"."destid")) OR
-	// 			  ("sg"."sourceid" IN ("e"."sourceid", "e"."destid"))
-	// 			 )
-	//  		  WHERE (("e"."destkind" != 'Node') AND
-	// 				 ("sg"."level" <=3)
-	//  				)
-	// 		 )
-	// 		) SELECT DISTINCT "level", "sourceid", "destid", "sourcekind", "destkind", "cluster" FROM "search_graph"
-	// 	) AS "search_graph"
-	// ) AS "combineIds"
-	// WHERE (("level" <=3)
-	// AND ("uid" NOT IN ('local-cluster/108a77a2-159c-4621-ae1e-7a3649000ebc')))
-	// GROUP BY "uid", "kind"
-	// -- union -- This is added if `kind:Cluster` is present in search term
-	//-- select uid as uid, data->>'kind' as kind, 1 AS "level" FROM search.resources where cluster IN ('local-cluster')
+	/**
+	Example query to find relations between resources - accepts an array of uids
+	=============================================================================
+	SELECT "uid", "kind", MIN("level") AS "level" FROM
+	(
+		SELECT "level", unnest(array[sourceid, destid, concat('cluster__',cluster)]) AS "uid",
+	unnest(array[sourcekind, destkind, 'Cluster']) AS "kind"
+		FROM (
+			WITH RECURSIVE search_graph(level, sourceid, destid,  sourcekind, destkind, cluster) AS
+			(SELECT 1 AS "level", "sourceid", "destid", "sourcekind", "destkind", "cluster"
+			 FROM "search"."edges" AS "e"
+			 WHERE (("destid" IN ('local-cluster/108a77a2-159c-4621-ae1e-7a3649000ebc' )) OR
+						("sourceid" IN ('local-cluster/108a77a2-159c-4621-ae1e-7a3649000ebc'))
+				   )
+					   UNION
+			 (SELECT level+1 AS "level", "e"."sourceid", "e"."destid", "e"."sourcekind", "e"."destkind", "e"."cluster"
+			  FROM "search"."edges" AS "e"
+			  INNER JOIN "search_graph" AS "sg"
+			  ON (("sg"."destid" IN ("e"."sourceid", "e"."destid")) OR
+				  ("sg"."sourceid" IN ("e"."sourceid", "e"."destid"))
+				 )
+	 		  WHERE (("e"."destkind" != 'Node') AND
+					 ("sg"."level" <=3)
+	 				)
+			 )
+			) SELECT DISTINCT "level", "sourceid", "destid", "sourcekind", "destkind", "cluster" FROM "search_graph"
+		) AS "search_graph"
+	) AS "combineIds"
+	WHERE (("level" <=3)
+	AND ("uid" NOT IN ('local-cluster/108a77a2-159c-4621-ae1e-7a3649000ebc')))
+	GROUP BY "uid", "kind"
+	-- union -- This is added if `kind:Cluster` is present in search term
+	-- select uid as uid, data->>'kind' as kind, 1 AS "level" FROM search.resources where cluster IN ('local-cluster')
+	*/
 	s.setDepth()
 	whereDs := []exp.Expression{
 		goqu.C("level").Lte(s.level), // Add filter to select up to level (default 3) relationships
@@ -197,25 +192,26 @@ func (s *SearchResult) selectIfClusterUIDPresent() *goqu.SelectDataset {
 	}
 }
 
-func (s *SearchResult) buildRelatedKindsQuery() {
-	klog.V(3).Infof("Resolving relationships for [%d] uids.\n", len(s.uids))
+// Builds the query to get resource data from the relationships UIDs.
+func (s *SearchResult) buildQueryToGetItemsFromUIDs() {
+	klog.V(3).Infof("Building query to get items for [%d] uids.\n", len(s.uids))
 	var params []interface{}
 	var sql string
 	var err error
-	//define schema table:
+	// define schema table
 	schemaTable := goqu.S("search").Table("resources")
 	ds := goqu.From(schemaTable)
 
-	//SELECT CLAUSE
+	// SELECT CLAUSE
 	selectDs := ds.Select("uid", "cluster", "data")
 
-	//WHERE CLAUSE
+	// WHERE CLAUSE
 	whereDs := []exp.Expression{goqu.C("uid").In(s.uids)} // Add filter to avoid selecting the search object itself
 
-	//LIMIT CLAUSE
+	// LIMIT CLAUSE
 	limit := s.setLimit()
 
-	//Get the query
+	// Get the query
 	if limit != 0 {
 		sql, params, err = selectDs.Where(whereDs...).Limit(uint(limit)).ToSQL()
 	} else {
@@ -230,11 +226,11 @@ func (s *SearchResult) buildRelatedKindsQuery() {
 	s.params = params
 }
 
-func (s *SearchResult) getRelations(ctx context.Context) []SearchRelatedResult {
+func (s *SearchResult) getRelationResolvers(ctx context.Context) []SearchRelatedResult {
 	klog.V(3).Infof("Resolving relationships for [%d] uids.\n", len(s.uids))
 	relatedSearch := []SearchRelatedResult{}
 
-	//defining variables
+	// defining variables
 	relatedMap := map[string][]string{} // Map to store relations
 	if s.context == nil {
 		s.context = ctx
@@ -262,88 +258,70 @@ func (s *SearchResult) getRelations(ctx context.Context) []SearchRelatedResult {
 		s.updateKindMap(uid, kind, relatedMap) // Store result in a map
 	}
 
-	// retrieve provided kind filters only
-	if len(s.input.RelatedKinds) > 0 {
-		s.relatedKindUIDs(relatedMap) // get uids for provided kinds
-		// Build Related kinds query
-		s.buildRelatedKindsQuery()
-		items, err := s.resolveItems() // Fetch the related kind items
-		if err != nil {
-			klog.Warning("Error resolving relatedKind items", err)
-		} else { // Convert to (kind, items) format - when relatedKinds are requested
-			relatedSearch = s.searchRelatedResultKindItems(items)
-		}
-	} else { // Retrieve kind and count of related items
-		relatedSearch = s.searchRelatedResultKindCount(relatedMap)
+	// get uids for related items that match the relatedKind filter.
+	s.filterRelatedUIDs(relatedMap)
+
+	// Build query to get full item data from s.uids
+	s.buildQueryToGetItemsFromUIDs()
+	items, err := s.resolveItems() // Fetch the related items
+	if err != nil {
+		klog.Warning("Error resolving related items.", err)
+		return []SearchRelatedResult{}
 	}
-	klog.V(5).Info("relatedSearch: ", relatedSearch)
+
+	// Convert to format of the relationships resolver []SearchRelatedResult{kind, count, items}
+	relatedSearch = s.searchRelatedResultKindItems(items)
+
+	klog.V(5).Info("RelatedSearch Result: ", relatedSearch)
 	return relatedSearch
 }
 
-func (s *SearchResult) relatedKindUIDs(levelsMap map[string][]string) {
+// Filters the related UIDs to match the relatedKinds input.
+func (s *SearchResult) filterRelatedUIDs(levelsMap map[string][]string) {
 	klog.V(6).Info("levelsMap in relatedKindUIDs: ", levelsMap)
 
-	relatedKinds := pointerToStringArray(s.input.RelatedKinds)
 	s.uids = []*string{}
-	keys := getKeys(levelsMap)
-	for _, kind := range relatedKinds {
-		// Convert kind to right case even if incoming query in RelatedKinds is all lowercase
-		// Needed for V1 compatibility.
-		for _, key := range keys {
-			if strings.EqualFold(key, kind) {
-				s.uids = append(s.uids, stringArrayToPointer(levelsMap[key])...)
-				break
+
+	// If relatedKinds filter is empty, include all.
+	if s.input.RelatedKinds == nil || len(s.input.RelatedKinds) == 0 {
+		for _, values := range levelsMap {
+			s.uids = append(s.uids, stringArrayToPointer(values)...)
+		}
+	} else {
+		// Only include UIDs of related items that match the relatedKinds filter.
+		kinds := getKeys(levelsMap)
+		for _, kindFilter := range s.input.RelatedKinds {
+			for _, kind := range kinds {
+				if strings.EqualFold(kind, *kindFilter) {
+					s.uids = append(s.uids, stringArrayToPointer(levelsMap[kind])...)
+					break
+				}
 			}
 		}
+		if len(s.uids) == 0 {
+			klog.Warning("No UIDs matched for relatedKinds: ", pointerToStringArray(s.input.RelatedKinds))
+		}
 	}
-	klog.V(6).Info("Number of relatedKind UIDs: ", len(s.uids))
-	if len(s.uids) == 0 && len(s.input.RelatedKinds) > 0 {
-		klog.Warning("No UIDs matched for relatedKinds: ", pointerToStringArray(s.input.RelatedKinds))
-	}
-}
 
-func (s *SearchResult) searchRelatedResultKindCount(levelMap map[string][]string) []SearchRelatedResult {
-
-	relatedSearch := make([]SearchRelatedResult, len(levelMap))
-
-	i := 0
-	//iterating and sending values to relatedSearch
-	for kind, uidArray := range levelMap {
-		count := len(uidArray)
-		relatedSearch[i] = SearchRelatedResult{Kind: kind, Count: &count}
-		i++
-	}
-	return relatedSearch
+	klog.V(6).Info("Number of related UIDs after filtering relatedKinds: ", len(s.uids))
 }
 
 func (s *SearchResult) searchRelatedResultKindItems(items []map[string]interface{}) []SearchRelatedResult {
-
-	relatedSearch := make([]SearchRelatedResult, 0)
-	relatedItems := map[string][]map[string]interface{}{}
-	relatedKinds := pointerToStringArray(s.input.RelatedKinds)
-
-	//iterating and sending values to relatedSearch
+	// Organize the related items by kind.
+	relatedItemsByKind := map[string][]map[string]interface{}{}
 	for _, currItem := range items {
-		currKind := currItem["kind"].(string)
-		for _, relKind := range relatedKinds {
-			if strings.EqualFold(relKind, currKind) {
-				// Convert kind to right case if incoming query in RelatedKinds is all lowercase
-				// Needed for V1 compatibility.
-				kindItemList := relatedItems[relKind]
-				currItem["kind"] = relKind
-				kindItemList = append(kindItemList, currItem)
-				relatedItems[relKind] = kindItemList
-				break
-			}
-		}
-
+		kind := currItem["kind"].(string)
+		kindItemList := relatedItemsByKind[kind]
+		relatedItemsByKind[kind] = append(kindItemList, currItem)
 	}
 
-	//iterating and sending values to relatedSearch
-	for kind, items := range relatedItems {
-		relatedSearch = append(relatedSearch, SearchRelatedResult{Kind: kind, Items: items})
+	// Generate result for each kind.
+	result := make([]SearchRelatedResult, 0)
+	for kind, items := range relatedItemsByKind {
+		count := len(items)
+		result = append(result, SearchRelatedResult{Kind: kind, Items: items, Count: &count})
 	}
-	return relatedSearch
+	return result
 }
 
 func (s *SearchResult) updateKindMap(uid string, kind string, levelMap map[string][]string) {
@@ -391,7 +369,6 @@ func (s *SearchResult) searchApplication() bool {
 }
 
 func stringArrayToPointer(stringArray []string) []*string {
-
 	values := make([]*string, len(stringArray))
 	for i, val := range stringArray {
 		tmpVal := val
