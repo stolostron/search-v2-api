008bb9f40e6bf0d1aeccc69f8367a1dc4dd7f52b
Jorge Padilla <jpadilla@redhat.com>
2022-10-27 00:44:48 -0400

    Initial SlowLog tests
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

---
+++ pkg/metric/slowLog_test.go
@@ -0,0 +1,42 @@
+// Copyright Contributors to the Open Cluster Management project
+
+package metric
+
+import (
+	"testing"
+	"time"
+	// klog "k8s.io/klog/v2"
+)
+
+// Should use default value when environment variable does not exist.
+func Test_SlowLog_Default(t *testing.T) {
+	// Mock console
+
+	// Set env SLOW_LOG
+
+	endFn := SlowLog("Test", 0)
+	time.Sleep(100)
+	endFn()
+
+	// Verify console log is called.
+}
+
+func Test_SlowLog_CustomDuration(t *testing.T) {
+	// Mock console
+
+	endFn := SlowLog("Test", 5*time.Millisecond)
+	time.Sleep(10)
+	endFn()
+
+	// Verify console is called.
+}
+
+func Test_SlowLog_ThreasholdNotMet(t *testing.T) {
+	// Mock console
+
+	endFn := SlowLog("Test", 5*time.Millisecond)
+	time.Sleep(1)
+	endFn()
+
+	// Verify console log not called.
+}
