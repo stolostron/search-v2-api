2abc6704e5e01474e7c03d6c7b8b18ce181f2303
Jorge Padilla <jpadilla@redhat.com>
2022-11-02 22:39:26 -0400

    Add missing tests
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/rbac/cacheMetadata_test.go
+++ pkg/rbac/cacheMetadata_test.go
@@ -1,8 +1,10 @@
 package rbac
 
 import (
-	"testing"
+	"errors"
 	"time"
+	// "fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,6 +15,24 @@ func Test_cache_isValid(t *testing.T) {
 	}
 
 	assert.True(t, mock.isValid())
+}
+
+func Test_cache_isValid_expired(t *testing.T) {
+	mock := cacheMetadata{
+		updatedAt: time.Now().Add(-3 * time.Minute),
+	}
+
+	assert.False(t, mock.isValid())
+}
+
+func Test_cache_isValid_withError(t *testing.T) {
+	mock := cacheMetadata{
+		err:       errors.New("Some error"),
+		updatedAt: time.Now().Add(-990 * time.Millisecond),
+	}
+	assert.True(t, mock.isValid())
+	time.Sleep(20 * time.Millisecond)
+	assert.False(t, mock.isValid())
 }
 
 func Test_cache_isValid_customTTL(t *testing.T) {
