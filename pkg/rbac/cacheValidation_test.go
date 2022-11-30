86e63cb0a0f5e8adb8f732e0678c85f96ec36bde
Jorge Padilla <jpadilla@redhat.com>
2022-11-29 22:48:48 -0500

    NamespaceAdded tests
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/rbac/cacheValidation_test.go
+++ pkg/rbac/cacheValidation_test.go
@@ -8,6 +8,32 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
+func Test_cacheValidation_namespaceAdded(t *testing.T) {
+	mock_cache := Cache{
+		shared: SharedData{
+			namespaces:       []string{"a", "b"},
+			managedClusters:  map[string]struct{}{"a": {}, "b": {}},
+			disabledClusters: map[string]struct{}{"a": {}, "b": {}},
+		},
+	}
+
+	mock_namespace := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "Namespace",
+			"metadata": map[string]interface{}{
+				"name": "c",
+			},
+		},
+	}
+
+	mock_cache.namespaceAdded(mock_namespace)
+
+	assert.Equal(t, []string{"a", "b", "c"}, mock_cache.shared.namespaces)
+	// assert.Equal(t, map[string]struct{}{"a": {}, "b": {}, "c": {}}, mock_cache.shared.disabledClusters)
+	// assert.Equal(t, map[string]struct{}{"a": {}, "b": {}, "c": {}}, mock_cache.shared.managedClusters)
+}
+
 func Test_cacheValidation_namespaceDeleted(t *testing.T) {
 	mock_cache := Cache{
 		shared: SharedData{
@@ -29,7 +55,7 @@ func Test_cacheValidation_namespaceDeleted(t *testing.T) {
 
 	mock_cache.namespaceDeleted(mock_namespace)
 
-	assert.Equal(t, mock_cache.shared.namespaces, []string{"b"})
-	assert.Equal(t, mock_cache.shared.disabledClusters, map[string]struct{}{"b": {}})
-	assert.Equal(t, mock_cache.shared.managedClusters, map[string]struct{}{"b": {}})
+	assert.Equal(t, []string{"b"}, mock_cache.shared.namespaces)
+	assert.Equal(t, map[string]struct{}{"b": {}}, mock_cache.shared.disabledClusters)
+	assert.Equal(t, map[string]struct{}{"b": {}}, mock_cache.shared.managedClusters)
 }
