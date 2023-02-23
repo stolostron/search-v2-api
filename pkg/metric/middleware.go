b699f159dae364c8972148c8d83c57126c083aff
Anxhela <acoba@redhat.com>
2023-02-23 16:14:55 -0500

    uncomment httprequesttotal
    Signed-off-by: Anxhela <acoba@redhat.com>

--- pkg/metric/middleware.go
+++ pkg/metric/middleware.go
@@ -6,6 +6,7 @@ import (
 	klog "k8s.io/klog/v2"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 // prometheusMiddleware implements mux.MiddlewareFunc.
@@ -17,9 +18,12 @@ func PrometheusMiddleware(next http.Handler) http.Handler {
 		klog.V(4).Infof("User Agent: %s", r.UserAgent())
 		//Not working as intended. This does not capture the time taken by the entire call.
 		//it only captures the time taken inside this call.
-		/* timer := prometheus.NewTimer(HttpDuration.WithLabelValues(path, r.Method))
-		timer.ObserveDuration()*/
+
+		timer := prometheus.NewTimer(HttpDuration.WithLabelValues(path, r.Method))
+		defer timer.ObserveDuration()
+
 		HttpRequestTotal.WithLabelValues(path, r.Method).Inc()
+
 		next.ServeHTTP(w, r)
 
 	})
