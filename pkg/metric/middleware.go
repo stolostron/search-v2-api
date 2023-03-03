4b8f8142a17fbbc63260b462947a029fd10d875e
Jorge Padilla <jpadilla@redhat.com>
2023-03-03 17:03:51 -0500

    Update Prom middleware to capture status_code after processing
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/metric/middleware.go
+++ pkg/metric/middleware.go
@@ -6,21 +6,41 @@ import (
 	klog "k8s.io/klog/v2"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+type metricsResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *metricsResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
 
 // prometheusMiddleware implements mux.MiddlewareFunc.
 func PrometheusMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		klog.Info("At PrometheusMiddleware BEFORE processing the request.")
 		route := mux.CurrentRoute(r)
 		path, _ := route.GetPathTemplate()
-		klog.V(4).Infof("URL Referrer: %s", r.Referer())
-		klog.V(4).Infof("User Agent: %s", r.UserAgent())
-		//Not working as intended. This does not capture the time taken by the entire call.
-		//it only captures the time taken inside this call.
-		/* timer := prometheus.NewTimer(HttpDuration.WithLabelValues(path, r.Method))
-		timer.ObserveDuration()*/
-		HttpRequestTotal.WithLabelValues(path, r.Method).Inc()
-		next.ServeHTTP(w, r)
 
+		timer := prometheus.NewTimer(HttpDuration.WithLabelValues(path, r.Method))
+		HttpRequestTotal.WithLabelValues(path, r.Method).Inc()
+
+		metricsRespWriter := NewMetricsResponseWriter(w)
+
+		// This will run at the end of the middleware chain.
+		defer func() {
+			klog.Infof("At prom middleware AFTER processing the request. response_code  %+v", metricsRespWriter.statusCode)
+			timer.ObserveDuration()
+		}()
+
+		next.ServeHTTP(metricsRespWriter, r)
 	})
 }
