1decddecad6bc1b575bb0cc9a7146a006d76afc6
Jorge Padilla <jpadilla@redhat.com>
2023-03-07 16:42:04 -0500

    Remove unused code
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/metric/middleware.go
+++ pkg/metric/middleware.go
@@ -6,20 +6,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type metricsResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func NewMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
-	return &metricsResponseWriter{w, 0}
-}
-
-func (lrw *metricsResponseWriter) WriteHeader(code int) {
-	lrw.statusCode = code
-	lrw.ResponseWriter.WriteHeader(code)
-}
-
 // prometheusMiddleware implements mux.MiddlewareFunc.
 func PrometheusMiddleware(next http.Handler) http.Handler {
 
