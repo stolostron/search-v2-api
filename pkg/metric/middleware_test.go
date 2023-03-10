173236985296a1354f5a6fd4c0cfca5cbcc828af
Jorge Padilla <jpadilla@redhat.com>
2023-03-10 16:35:20 -0500

    Tidy up test
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/metric/middleware_test.go
+++ pkg/metric/middleware_test.go
@@ -7,65 +7,43 @@ import (
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+	// "github.com/prometheus/client_golang/prometheus/promauto"
 	"gotest.tools/assert"
 )
 
-func TestDurationCode(t *testing.T) {
-
+func Test_DurationCode(t *testing.T) {
 	registry := prometheus.NewRegistry()
 
-	// mock http handler:
+	// mock http handler
 	httpHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
-
-	//mock HistogramVec
-	durationHistogram := promauto.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name: "http_request_duration_seconds_test",
-			Help: "Duration of my request in seconds.",
-		},
-		[]string{"code"},
-	)
-	registry.MustRegister(durationHistogram)
-
-	promMiddle := PrometheusMiddleware(httpHandler)
-
-	//create a mock resquest to pass to handler:
-	r, err := http.NewRequest("GET", "https://localhost:4010/searchapi/graphql", nil)
-	if err != nil {
-		t.Fatal(err)
-
-	}
+	// create a mock resquest to pass to handler
+	r := httptest.NewRequest("POST", "https://localhost:4010/searchapi/graphql", nil)
 	// create response recorder so we can get status code and serve mock request
 	resp := httptest.NewRecorder()
+
+	// Execute middleware function.
+	promMiddle := PrometheusMiddleware(httpHandler)
 	promMiddle.ServeHTTP(resp, r)
 
+	// Validate result.
 	// use the prometheus registry to confirm metrics have been scraped:
 	metricFamilies, _ := registry.Gather()
-
 	for _, mf := range metricFamilies {
-
 		//assert our metric got collected:
 		assert.Equal(t, mf.GetName(), "http_request_duration_seconds_test")
 
-		//assert only one metric: probably redundant
-		// assert.Equal(t, 1, len(mf.GetMetric()))
-
 		for _, v := range mf.Metric[0].GetLabel() {
-
-			//assert label code is 200
+			// assert label code is 200
 			if *v.Name == "code" {
 				val := *v.Value
 				assert.Equal(t, strconv.Itoa(resp.Code), val)
 			}
-
 		}
 
 		for _, m := range mf.GetMetric() {
-			//assert count of metric is one (one request)
+			// assert count of metric is one (one request)
 			assert.Equal(t, uint64(1), m.GetHistogram().GetSampleCount())
 		}
-
 	}
 
 }
