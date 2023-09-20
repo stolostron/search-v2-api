b22cd36718f7efe2e4d8642989d075f66980d6f9
Sherin Varughese <shvarugh@redhat.com>
2023-09-20 17:15:12 -0400

    update tests
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- pkg/rbac/dbAvailability_test.go
+++ pkg/rbac/dbAvailability_test.go
@@ -17,9 +17,8 @@ func TestCheckDBAvailabilityError(t *testing.T) {
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 
-	cache := newMockCache()
 	// execute function
-	result := cache.CheckDBAvailability(handler)
+	result := CheckDBAvailability(handler)
 
 	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
 	// directly and pass in our Request and ResponseRecorder.
@@ -34,35 +33,5 @@ func TestCheckDBAvailabilityError(t *testing.T) {
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got *%v* want *%v*",
 			rr.Body.String(), expected)
-	}
-}
-
-func TestCheckDBAvailabilityPass(t *testing.T) {
-	// Create a request to pass to our handler.
-	req, err := http.NewRequest("GET", "/search", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
-
-	cache := newMockCache()
-	cache.setDbConnInitialized(true)
-	// execute function
-	result := cache.CheckDBAvailability(handler)
-
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	result.ServeHTTP(rr, req)
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-	// Check the response body is what we expect.
-	if rr.Body.String() != "" {
-		t.Errorf("handler returned unexpected body: got *%v* want *%v*",
-			rr.Body.String(), "")
 	}
 }
