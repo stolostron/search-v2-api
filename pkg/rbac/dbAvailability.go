077846828a07106e3fe8ca2feb624c7f059cfbf4
Sherin Varughese <shvarugh@redhat.com>
2023-09-20 19:08:28 -0400

    Refresh cache for all requests (#180)
    * refresh cache for each request
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * update tests
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    ---------
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- pkg/rbac/dbAvailability.go
+++ pkg/rbac/dbAvailability.go
@@ -6,8 +6,9 @@ import (
 	"k8s.io/klog/v2"
 )
 
-func (c *Cache) CheckDBAvailability(next http.Handler) http.Handler {
+func CheckDBAvailability(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c := GetCache()
 		// if postgres db is not setup, return error
 		if !c.dbConnInitialized {
 			klog.Warning("Unable to handle request because we couldn't establish connection with database.")
