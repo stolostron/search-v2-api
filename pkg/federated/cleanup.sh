042c71df7cfc9359b60118d3a1e516f7b0c748f8
Jorge Padilla <jpadilla@redhat.com>
2024-01-17 15:44:32 -0500

    Simplify setup; disable client pool
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

---
+++ pkg/federated/cleanup.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# Copyright Contributors to the Open Cluster Management project
+
+# Delete resources created by setup.sh
+MANAGED_HUBS=($(oc get managedcluster -o json | jq -r '.items[] | select(.status.clusterClaims[] | .name == "hub.open-cluster-management.io" and .value != "NotInstalled") | .metadata.name'))
+
+for MANAGED_HUB in "${MANAGED_HUBS[@]}"; do
+  oc delete -n ${MANAGED_HUB} -f ./federation-managed-hub-config.yaml
+done 
+oc delete -f ./federation-config.yaml
