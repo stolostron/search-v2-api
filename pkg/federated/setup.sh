af4fd0c8e8a97176d74848c354b4cde400c5dd0f
Jorge Padilla <jpadilla@redhat.com>
2024-01-18 15:30:07 -0500

    Simplify ManagedServiceAccount enablement
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/federated/setup.sh
+++ pkg/federated/setup.sh
@@ -5,16 +5,26 @@
 
 # TODO: In the future this should be further automated by either Search or Global Hub operator.
 
-# Install the Managed Service Account addon using Helm.
+# Enable the Managed Service Account addon in the MultiClusterEngine CR.
 oc get managedserviceaccount > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-  echo "Managed Service Account addon already installed."
+  echo "Managed Service Account add-on is enabled."
 else
-  echo "Installing Managed Service Account addon..."
-  helm repo add ocm https://openclustermanagement.blob.core.windows.net/releases/
-  helm repo update
-  helm search repo ocm/managed-serviceaccount
-  helm install -n open-cluster-management-addon --create-namespace managed-serviceaccount ocm/managed-serviceaccount
+  oc patch multiclusterengine multiclusterengine --type='json' -p='[{"op": "add", "path": "/spec/overrides/components", "value": [{"name": "managedserviceaccount", "enabled": true}]}]'
+  echo "Enabled Managed Service Account add-on in MulticlusterEngine. Waiting for the addon to be installed..."
+  
+  # Wait for the Managed Service Account addon to be installed.
+  i=0
+  while [ $i -lt 30 ]; do
+    echo -n "."
+    sleep 1
+    i=$((i+1))
+    oc get managedserviceaccount > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      echo "Managed Service Account add-on is enabled."
+      break
+    fi
+  done
 fi
 
 # Create local config resources for Global Search.
@@ -24,7 +34,12 @@ oc apply -f ./federation-config.yaml
 # Create config resources for each Managed Hub.
 echo "Creating config resources for each Managed Hub..."
 MANAGED_HUBS=($(oc get managedcluster -o json | jq -r '.items[] | select(.status.clusterClaims[] | .name == "hub.open-cluster-management.io" and .value != "NotInstalled") | .metadata.name'))
+if [ ${#MANAGED_HUBS[@]} -eq 0 ]; then
+  echo "No managed hubs found. Exiting."
+  exit 1
+fi
 for MANAGED_HUB in "${MANAGED_HUBS[@]}"; do
+  # TODO: Validate that the Managed Hub is running ACM 2.9.0 or later.
   echo "Configuring managed hub: ${MANAGED_HUB}"
   oc apply -f ./federation-managed-hub-config.yaml -n "${MANAGED_HUB}"
 done
