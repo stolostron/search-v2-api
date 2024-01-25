0c0e502ac2ae4c59f3cf517875a15a49dbb3f391
Jorge Padilla <jpadilla@redhat.com>
2024-01-25 16:14:06 -0500

    Improve setup.sh
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/federated/setup.sh
+++ pkg/federated/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Copyright Contributors to the Open Cluster Management project
 
-# Run this script on the Global Hub cluster to configure Global Search access to the Managed Hub clusters.
+# Configures Global Search.
+# Run this script on an Openshift cluster with the Multicluster Global Hub operator.
 
-# TODO: In the future this should be further automated by either Search or Global Hub operator.
+# FUTURE: The actions in this script should be moved to either Search or Global Hub operator.
+
 
 echo "Configuring Global Search on the following cluster."
 oc cluster-info | grep "Kubernetes"
@@ -20,16 +22,42 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   exit 1
 fi
 
-# Validate that MulticlusterGlobalHub is installed.
-oc get multiclusterglobalhub > /dev/null 2>&1
+echo -e "Validating prerequisites...\n"
+
+# Validate that Red Hat Advanced Cluster Management (MulticlusterHub) 2.10.0 is installed.
+REQUIRED_RHACM_VERSION="2.10.0"
+oc get multiclusterhub -o yaml > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "ERROR: MulticlusterGlobalHub is not installed in the target cluster. Exiting."
+  echo "❌ ERROR: Red Hat Advanced Cluster Management (MulticlusterHub) is not installed in the target cluster. Exiting."
   exit 1
+else
+  rhacm_version=$(oc get multiclusterhub -A -o yaml | yq '.items[0].status.currentVersion')
+  if ! printf "${REQUIRED_RHACM_VERSION}\n%s\n" $rhacm_version | sort -V -C; then
+    echo "❌ ERROR: Red Hat Advanced Cluster Management (MulticlusterHub) version ${rhacm_version} found, but ${REQUIRED_RHACM_VERSION} is required. Exiting."
+    exit 1
+  fi
+  echo "✅ Red Hat Advanced Cluster Management (MulticlusterHub) version ${rhacm_version} found."
 fi
 
-echo "Enabling global search feature in the console and search-api..."
+# Validate that MulticlusterGlobalHub 1.0.1 is installed.
+REQUIRED_MCGH_VERSION="1.0.1"
+oc get multiclusterglobalhub -o yaml > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "❌ ERROR: Multicluster Global Hub is not installed in the target cluster. Exiting."
+  exit 1
+else
+  mcgh_version=$(oc get multiclusterglobalhub -A -o yaml | yq '.items[0].status.currentVersion')
+  if ! printf "${REQUIRED_MCGH_VERSION}\n%s\n" $rhacm_version | sort -V -C; then
+    echo "❌ ERROR: Multicluster Global Hub version ${rhacm_version} found, but ${REQUIRED_MCGH_VERSION} is required. Exiting."
+    exit 1
+  fi
+  echo "✅ Multicluster Global Hub version ${mcgh_version} found."
+fi
+
+echo -e "\nEnabling the global search feature in the console and search-api...\n"
 # Enable global search feature in the console.
 oc patch configmap console-mce-config -n multicluster-engine -p '{"data": {"globalSearchFeatureFlag": "enabled"}}'
+oc patch configmap console-mce-config -n multicluster-engine -p '{"data": {"searchApiEndpoint": "/federated"}}'
 # Enable federated search feature in the search-api.
 oc patch search search-v2-operator -n open-cluster-management --type='merge' -p '{"spec":{"deployments":{"queryapi":{"envVar":[{"name":"FEATURE_FEDERATED_SEARCH", "value":"true"}]}}}}'
 
@@ -50,17 +78,17 @@ else
     i=$((i+1))
     oc get managedserviceaccount > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-      echo "Managed Service Account add-on is enabled."
+      echo -e "\nManaged Service Account add-on is enabled."
       break
     fi
   done
 fi
 
 # Create config resources for each Managed Hub.
-echo "Creating config resources for each Managed Hub..."
+echo -e "\nCreating config resources for each Managed Hub...\n"
 MANAGED_HUBS=($(oc get managedcluster -o json | jq -r '.items[] | select(.status.clusterClaims[] | .name == "hub.open-cluster-management.io" and .value != "NotInstalled") | .metadata.name'))
 if [ ${#MANAGED_HUBS[@]} -eq 0 ]; then
-  echo "No managed hubs found. Exiting."
+  echo "❌ No managed hubs found. Exiting."
   exit 1
 fi
 for MANAGED_HUB in "${MANAGED_HUBS[@]}"; do
@@ -68,4 +96,7 @@ for MANAGED_HUB in "${MANAGED_HUBS[@]}"; do
   # ACM_VERSION=$(oc get mch -A -o custom-columns=VERSION:.status.currentVersion --no-headers)
   echo "Configuring managed hub: ${MANAGED_HUB}"
   oc apply -f ./federation-managed-hub-config.yaml -n "${MANAGED_HUB}"
+  echo ""
 done
+
+echo "🚀 Global Search setup complete."
