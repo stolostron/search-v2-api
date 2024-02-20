a180c30115bec09f5dc4691fe5253fab6452a1ba
Jorge Padilla <jpadilla@redhat.com>
2024-02-20 16:59:18 -0500

    Update federated search setup and readme (#206)
    * Update federated search setup and readme
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update readme
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update readme with feature status
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Add echo to setup script
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    ---------
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/federated/README.md
+++ pkg/federated/README.md
@@ -2,6 +2,8 @@
 
 Use federated search to query and combine results from multiple Red Hat Advanced Cluster Management.
 
+**Feature Status (2/20/2024): Development Preview**
+
 ## Pre-requisites  
 1. Multicluster Global Hub Operator 1.0.0 or later.  
 2. Red Hat Advanced Cluster Management 2.10.0 or later.  
@@ -31,3 +33,14 @@ Execute the script at `./cleanup.sh` to remove the Global Search configuration f
 ```bash
 bash <(curl -s https://raw.githubusercontent.com/stolostron/search-v2-api/main/pkg/federated/cleanup.sh) 
 ``` 
+
+## Limitations
+
+Known limitations for Development Preview.
+
+1. When a Managed Hub is added, resources won't appear on global search. Re-run ./setup.sh to update configuration.
+2. When a Managed Hub is offline, the console will show an error. Re-run ./setup.sh to resolve.
+3. Filter by Managed Hub is not implemented.
+4. LIMIT and SORT for aggregated results is not implemented.
+5. Resource YAML and log views are not implemented in the console. This is removed when Global Search is enabled.
+6. Actions on resources (delete, edit) are not implemented in the console. This is removed when Global Search is enabled.
--- pkg/federated/cleanup.sh
+++ pkg/federated/cleanup.sh
@@ -2,72 +2,11 @@
 # Copyright Contributors to the Open Cluster Management project
 
 # Delete resources created by setup.sh
-MANAGED_HUBS=($(oc get managedcluster -o json | jq -r '.items[] | select(.status.clusterClaims[] | .name == "hub.open-cluster-management.io" and .value != "NotInstalled") | .metadata.name'))
 
-for MANAGED_HUB in "${MANAGED_HUBS[@]}"; do
-  # NOTE: The YAML file is added in-line below to simplify deployment with a single script file.
-  #       Any changes below must be synchronized with the setup.sh script.
-  # oc delete -n ${MANAGED_HUB} -f ./federation-managed-hub-config.yaml
-  oc delete -n "${MANAGED_HUB}" -f - << EOF
-apiVersion: work.open-cluster-management.io/v1
-kind: ManifestWork
-metadata:
-  labels:
-    app: ocm-search
-  name: search-global-config
-spec:
-  workload:
-    manifests:
-    - apiVersion: rbac.authorization.k8s.io/v1
-      kind: ClusterRoleBinding
-      metadata:
-        labels:
-          app: ocm-search
-        name: search-global-binding
-      roleRef:
-        apiGroup: rbac.authorization.k8s.io
-        kind: ClusterRole
-        name: global-search-user
-      subjects:
-      - kind: ServiceAccount
-        name: search-global
-        namespace: open-cluster-management-agent-addon
-    - apiVersion: route.openshift.io/v1
-      kind: Route
-      metadata:
-        labels:
-          app: ocm-search
-        name: search-global-hub
-        namespace: open-cluster-management
-      spec:
-        port:
-          targetPort: search-api
-        tls:
-          termination: passthrough
-        to:
-          kind: Service
-          name: search-search-api
-          weight: 100
----
-apiVersion: addon.open-cluster-management.io/v1alpha1
-kind: ManagedClusterAddOn
-metadata:
-  name: managed-serviceaccount
-  labels:
-    app: ocm-search
-spec:
-  installNamespace: open-cluster-management-agent-addon
----
-apiVersion: authentication.open-cluster-management.io/v1beta1
-kind: ManagedServiceAccount
-metadata:
-  name: search-global
-  labels:
-    app: ocm-search
-spec:
-  rotation: {}
-EOF
-done 
+# Delete configuration resources for the Managed Hubs.
+oc delete manifestwork -A -l app=ocm-search
+oc delete managedclusteraddon -A -l app=ocm-search
+oc delete managedserviceaccount -A -l app=ocm-search
 
 # Disable global search feature in the console.
 oc patch configmap console-mce-config -n multicluster-engine -p '{"data": {"globalSearchFeatureFlag": "disabled"}}'
@@ -89,4 +28,3 @@ else
   echo "Disabled the Managed Service Account add-on in MulticlusterEngine."
   echo "✅ Global Search feature has been disabled and resources have been deleted." 
 fi
-
