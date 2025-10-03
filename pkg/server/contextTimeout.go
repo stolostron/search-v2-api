e69c050c23b6a120b353d03012fc4979d22eeb9a
Spencer McAvey <smcavey@redhat.com>
2025-10-03 15:45:58 -0400

    simplify requesttimeout middleware
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>

--- pkg/server/contextTimeout.go
+++ pkg/server/contextTimeout.go
@@ -14,15 +14,14 @@ import (
 func RequestTimeout(cfg config.Config) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			var ctx context.Context
-			var cancel context.CancelFunc
+			timeout := time.Duration(cfg.RequestTimeout) * time.Millisecond
+			requestType := "request"
 			if websocket.IsWebSocketUpgrade(r) {
-				ctx, cancel = context.WithTimeout(r.Context(), time.Duration(cfg.StreamRequestTimeout)*time.Millisecond)
-				klog.V(5).Infof("Timeout of %v applied to stream request context", time.Duration(cfg.StreamRequestTimeout)*time.Millisecond)
-			} else {
-				ctx, cancel = context.WithTimeout(r.Context(), time.Duration(cfg.RequestTimeout)*time.Millisecond)
-				klog.V(5).Infof("Timeout of %v applied to request context", time.Duration(cfg.RequestTimeout)*time.Millisecond)
+				timeout = time.Duration(cfg.StreamRequestTimeout) * time.Millisecond
+				requestType = "stream"
 			}
+			ctx, cancel := context.WithTimeout(r.Context(), timeout)
+			klog.V(5).Infof("Timeout of %v applied to %s context", timeout, requestType)
 			defer cancel()
 
 			next.ServeHTTP(w, r.WithContext(ctx))
