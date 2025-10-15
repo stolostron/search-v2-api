f626fd3fd89f02ca8bc73742192800d24266b12b
Spencer McAvey <smcavey@redhat.com>
2025-10-15 16:29:56 -0400

    bump timeout log verbosity v5->v8
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>

--- pkg/server/timeoutMiddleware.go
+++ pkg/server/timeoutMiddleware.go
@@ -10,7 +10,7 @@ import (
 func TimeoutHandler(timeout time.Duration) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			klog.V(5).Infof("Applying %v timeout for HTTP request: %s %s", timeout, r.Method, r.URL.Path)
+			klog.V(8).Infof("Applying %v timeout for HTTP request: %s %s", timeout, r.Method, r.URL.Path)
 			handler := http.TimeoutHandler(next, timeout, "Request timed out")
 			handler.ServeHTTP(w, r)
 		})
