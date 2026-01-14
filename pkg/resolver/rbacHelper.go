cf2f41312e3415213cecb633d7b0d212daa5958f
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2026-01-14 22:55:43 +0100

    rbac cs len check (#674)
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    Co-authored-by: Spencer McAvey <smcavey@redhat.com>

--- pkg/resolver/rbacHelper.go
+++ pkg/resolver/rbacHelper.go
@@ -61,7 +61,7 @@ func matchApigroupKind(resources []rbac.Resource) exp.ExpressionList {
 
 func matchClusterScopedResources(csRes []rbac.Resource, userInfo v1.UserInfo) exp.ExpressionList {
 	if len(csRes) == 0 {
-		return goqu.Or() // return empty clause
+		return goqu.And() // return false clause
 
 		// user has access to all cluster scoped resources
 	} else if len(csRes) == 1 && csRes[0].Apigroup == "*" && csRes[0].Kind == "*" {
