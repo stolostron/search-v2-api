cf33c4faddeaf74369b7c4386bac5eedb35686c0
Spencer McAvey <smcavey@redhat.com>
2026-01-27 15:58:59 -0500

    code coverage
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>

--- pkg/rbac/testutil_test.go
+++ pkg/rbac/testutil_test.go
@@ -1,9 +1,13 @@
 package rbac
 
 import (
-	"github.com/stretchr/testify/assert"
+	"context"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	authz "k8s.io/api/authorization/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestCreateTestContext(t *testing.T) {
@@ -49,5 +53,100 @@ func TestCreateTestUserDataCache(t *testing.T) {
 
 	// Then: it's made
 	assert.NotNil(t, userDataCache)
-	assert.Equal(t, len(userDataCache.UserData.UserPermissions.Items), 1)
+	assert.Equal(t, len(userDataCache.UserPermissions.Items), 1)
+}
+
+func TestMockSelfSubjectAccessReviewInterface_Create_Allowed(t *testing.T) {
+	// Given: a mock client with a permission that allows watch on pods in namespace foo
+	mockClient := NewMockAuthzClient()
+	mockClient.Permissions[WatchPermissionKey{
+		verb:      "watch",
+		apigroup:  "v1",
+		kind:      "pods",
+		namespace: "foo",
+	}] = &WatchPermissionEntry{
+		allowed:   true,
+		updatedAt: time.Now(),
+	}
+
+	mockInterface := mockSelfSubjectAccessReviewInterface{client: mockClient}
+
+	// When: we create an SSAR for a matching permission
+	ssar := &authz.SelfSubjectAccessReview{
+		Spec: authz.SelfSubjectAccessReviewSpec{
+			ResourceAttributes: &authz.ResourceAttributes{
+				Verb:      "watch",
+				Group:     "v1",
+				Resource:  "pods",
+				Namespace: "foo",
+			},
+		},
+	}
+
+	result, err := mockInterface.Create(context.Background(), ssar, metav1.CreateOptions{})
+
+	// Then: permission should be granted
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.Status.Allowed, true, "Expected permission to be allowed")
+}
+
+func TestMockSelfSubjectAccessReviewInterface_Create_Denied(t *testing.T) {
+	// Given: a mock client with a permission that denies delete on pods in namespace foo
+	mockClient := NewMockAuthzClient()
+	mockClient.Permissions[WatchPermissionKey{
+		verb:      "delete",
+		apigroup:  "v1",
+		kind:      "pods",
+		namespace: "foo",
+	}] = &WatchPermissionEntry{
+		allowed:   false,
+		updatedAt: time.Now(),
+	}
+
+	mockInterface := mockSelfSubjectAccessReviewInterface{client: mockClient}
+
+	// When: we create an SSAR for the denied permission
+	ssar := &authz.SelfSubjectAccessReview{
+		Spec: authz.SelfSubjectAccessReviewSpec{
+			ResourceAttributes: &authz.ResourceAttributes{
+				Verb:      "delete",
+				Group:     "v1",
+				Resource:  "pods",
+				Namespace: "foo",
+			},
+		},
+	}
+
+	result, err := mockInterface.Create(context.Background(), ssar, metav1.CreateOptions{})
+
+	// Then: permission should be denied
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.Status.Allowed, false, "Expected permission to be denied")
+}
+
+func TestMockSelfSubjectAccessReviewInterface_Create_NotFound(t *testing.T) {
+	// Given: a mock client with no permissions set
+	mockClient := NewMockAuthzClient()
+	mockInterface := mockSelfSubjectAccessReviewInterface{client: mockClient}
+
+	// When: we create an SSAR for a permission that doesn't exist
+	ssar := &authz.SelfSubjectAccessReview{
+		Spec: authz.SelfSubjectAccessReviewSpec{
+			ResourceAttributes: &authz.ResourceAttributes{
+				Verb:      "watch",
+				Group:     "v1",
+				Resource:  "pods",
+				Namespace: "foo",
+			},
+		},
+	}
+
+	result, err := mockInterface.Create(context.Background(), ssar, metav1.CreateOptions{})
+
+	// Then: permission should be denied (default behavior when permission not found)
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.Status.Allowed, false, "Expected permission to be denied when not found")
 }
